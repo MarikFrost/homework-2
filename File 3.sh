1d717963a3e0a52e7dcaa59ef6c320d813e4bf63
MarikFrostr <recruiterhrpartner4@gmail.com>
2021-07-14 02:01:47 +0300

    Скрипт переименовывающий файл test.txt в test2.txt

--- File 3.sh
+++ File 3.sh
@@ -1,10 +1,12 @@
+#Скрипт переименовывающий файл test.txt в test2.txt
+
 #!/bin/bash
-file=directory/text.txt 
-renamefile=directory/text2.txt
-if [ -e $file ]
+file=directory/text.txt  #test.txt в папке directory присваиваем переменной file
+renamefile=directory/text2.txt  #test2.txt в папке directory присваиваем переменной renamefile
+if [ -e $file ] #проверяем наличие файла test.txt
 then 
-    mv $file $renamefile
-    if [ -e $renamefile ]
+    mv $file $renamefile  #переименовуем файл
+    if [ -e $renamefile ] # проверяем переименовался ли файл
     then
         echo "Файл $file переименован в $renamefile"
     fi
