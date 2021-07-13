5f3d903c2d98701553776996689c566e0cdd4581
MarikFrostr <recruiterhrpartner4@gmail.com>
2021-07-13 23:19:11 +0300

    Cкрипт копирующий файл 0 в файл test.txt

--- File 2.sh
+++ File 2.sh
@@ -1,18 +1,20 @@
+#крипт копирующий файл 0 в файл test.txt
+
 #!/bin/bash
 
-firstdir=directory/0
-seconddir=directory/text.txt
+firstdir=directory/0    #Файл 0 который находится в папке directory записываем в переменную firstdir
+seconddir=directory/text.txt #Файл text.txt который находится в папке directory записываем в переменную seconddir
 
-if [ -e $firstdir ]
+if [ -e $firstdir ]  #проверяем существует ли файл 0, если да то .....
 then
 
-    if [ -e $seconddir ]
+    if [ -e $seconddir ] # проверяем существует ли файл text.txt если да, то ....
     then
-        cp $firstdir $seconddir
+        cp $firstdir $seconddir # копируем файл 0 в файл text.txt
 
-        if [ -s $seconddir ]
+        if [ -s $seconddir ]  # проверяем произошло ли копирование если да, то .....
         then
-            code --wait $seconddir
+            code --wait $seconddir  # открываем файл в редакторе
         else
             echo "Копирование не удалось"
         fi
