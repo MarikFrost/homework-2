7091985a651b9f0e9ab917a35b3d02a2989e7c4b
MarikFrostr <recruiterhrpartner4@gmail.com>
2021-07-14 14:01:19 +0300

    #скрипт меняющий права доступа к файлу one/two/test6.txt : только на выполнение пользователем

--- File 7.sh
+++ File 7.sh
@@ -1,4 +1,6 @@
+#скрипт меняющий права доступа к файлу one/two/test6.txt : только на выполнение пользователем
+
 #!/bin/bash
-touch one.txt two.txt test6.txt
-chmod 111 one.txt two.txt test6.txt
-ls -l one.txt two.txt test6.txt
+touch one.txt two.txt test6.txt #создаем файлы
+chmod 111 one.txt two.txt test6.txt  # ставим права доступа только на виполнение для пользователей
+ls -l one.txt two.txt test6.txt # просматриваем права доступа
